db097944819cb1806cb21cb7419a37cee83a5dfc
jongrover <hello@jonathangrover.com>
2017-01-18 17:13:34 -0500

    fix scrolling, add fontawesome and jquery smoothscroll and shake effects

--- javascript/dontlook.js
+++ javascript/dontlook.js
@@ -2,22 +2,26 @@
 // http://hhassan1230.github.io/CodePenImgs/imagesLesson1/DontLookAwayLogoFinal1.png
 // https://pbs.twimg.com/media/Ct-IZb5WgAAmR2t.jpg
 // http://hhassan1230.github.io/CodePenImgs/imagesLesson1/CoverB2.png
-$(window).on("load", function() {
-  $('.vr-view').addClass("expand");
-  $('.vr-goggles').addClass('fadeout expand-goggles');
-    $('.text').addClass('fadein');
-    // $('.vr-view').fadeTo(100, 0.2);
-    // $(".vr-frame").css("background","none");
-  
-    
 
+$(window).on("load", function() {
+  $('.vr-view').addClass('expand');
+  $('.vr-goggles').addClass('fadeout expand-goggles');
+  $('.text-panel').addClass('fadein');
 });
 
-  $('.vr-frame').smoothScroll({         
-    delegateSelector: '.down-arrow a'       
+jQuery.fn.shake = function() {
+  this.each(function(i) {
+    $(this).css({ "position": "relative" });
+    for (var x = 1; x <= 3; x++) {
+        $(this).animate({ left: -10 }, 100).animate({ left: 0 }, 100).animate({ left: 10 }, 100).animate({ left: 0 }, 100);
+    }
   });
+  return this;
+}
 
-  //$('.down-arrow a').click(function(event) {         event.preventDefault();         
- // var link = this;         
- //    $.smoothScroll({           scrollTarget: link.hash         });       });               var p1Opts = $p1.smoothScroll('options') || {};          p1Opts.speed = p1Opts.speed === 1;
-//overflow: hidden;
+$(function () {
+  $('section').smoothScroll({
+    delegateSelector: 'a.down-arrow'
+  });
+  $('a.down-arrow i').delay(2000).shake();
+});
