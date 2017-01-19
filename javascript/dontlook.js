20ff9a84f3cc138bf202f773fc6ab464c27cff45
hhassan1230 <hhassan@oswego.edu>
2017-01-18 23:44:11 -0500

    jon gov session mobile & scroll fix

--- javascript/dontlook.js
+++ javascript/dontlook.js
@@ -2,11 +2,18 @@
 // http://hhassan1230.github.io/CodePenImgs/imagesLesson1/DontLookAwayLogoFinal1.png
 // https://pbs.twimg.com/media/Ct-IZb5WgAAmR2t.jpg
 // http://hhassan1230.github.io/CodePenImgs/imagesLesson1/CoverB2.png
+var delayTime = 4400;
+// fullanimtime / 3;
 
 $(window).on("load", function() {
   $('.vr-view').addClass('expand');
   $('.vr-goggles').addClass('fadeout expand-goggles');
   $('.text-panel, .down-arrow').addClass('fadein');
+  $('body').delay(delayTime).removeClass('no-scroll').addClass('scroll');
+  // $('.vr-view').delay(delayTime+5000).addClass('vr-view-shift-up');
+  setTimeout(function(){
+	  $('.vr-view').addClass('vr-view-shift-up');
+  }, delayTime+2000);
 });
 
 jQuery.fn.shake = function() {
@@ -20,6 +27,8 @@ jQuery.fn.shake = function() {
 }
 
 $(function () {
+  $(this).scrollTop(0);
+	$('body').addClass('no-scroll');
   $('section').smoothScroll({
     delegateSelector: 'a.down-arrow'
   });
